edac11cbda0500f1ccea6668fdb38670d9ba595a
zuoyonghui <zzhattzzh@126.com>
2025-01-04 02:03:13 +0800

    refactor: Remove debug statements from buffer creation logic

--- lua/aider-nvim/chat/buffer.lua
+++ lua/aider-nvim/chat/buffer.lua
@@ -48,8 +48,6 @@ function M.create()
 
   -- Save original buffer and cursor position
   original_buf = vim.api.nvim_get_current_buf()
-  dd(original_buf)
-  dd"original_buf"
   original_cursor_pos = vim.api.nvim_win_get_cursor(0)
 
   -- Clear previous selection
@@ -78,7 +76,6 @@ function M.create()
       end_line = end_line,
       content = numbered_lines
     }
-    dd(original_visual_selection)
   end
 
   local on_confirm = function(value)
